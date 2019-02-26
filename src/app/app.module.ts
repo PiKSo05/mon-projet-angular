929eb934e8b102749b52c88675b9cc685dc9b85a
PiKSo05 <vilain.vincent@gmail.com>
2019-02-26 22:40:57 +0100

    ajout de guard et de redirection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,16 @@ import { AppareilViewComponent } from './appareil-view/appareil-view.component'
 import {Routes, RouterModule} from "@angular/router"
 import { AuthService } from './Services/auth.service';
 import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+import { AuthGuard } from './Services/auth-guard.service';
 
 const appRoutes:Routes = [
-  {path: 'appareils', component:AppareilViewComponent},
-  {path: 'appareils/:id', component:SingleAppareilComponent},
+  {path: 'appareils', canActivate: [AuthGuard], component:AppareilViewComponent},
+  {path: 'appareils/:id',canActivate: [AuthGuard], component:SingleAppareilComponent},
   {path: 'auth', component:AuthComponent},
   {path: '', component:AppareilViewComponent},
+  {path: 'not-found', component:FourOhFourComponent},
+  {path: '**', redirectTo: 'not-found'},
 ]
 
 
@@ -26,7 +30,8 @@ const appRoutes:Routes = [
     AppareilComponent,
     AuthComponent,
     AppareilViewComponent,
-    SingleAppareilComponent
+    SingleAppareilComponent,
+    FourOhFourComponent
   ],
   imports: [
     BrowserModule,
@@ -36,7 +41,8 @@ const appRoutes:Routes = [
   ],
   providers: [
     AppareilService,
-    AuthService
+    AuthService,
+    AuthGuard
   ],
   bootstrap: [AppComponent]
 })
